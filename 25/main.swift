e4dcc5d683da0a52171e453fc21afb8f5109cf97
Mạnh Hùng <dimalantamanhhung11@gmail.com>
2025-04-27 09:11:55 +0700

    Initial Commit

---
+++ 25/main.swift
@@ -0,0 +1,31 @@
+//CÂU LỆNH ĐIỀU KIỆN --> BOOLEAN
+//CÚ PHÁP
+//var score = 10
+////-->true/false
+//if (score > 11) {
+//    print ("You passed")
+//}else if (score > 12){
+//    print ("Lon hon 12")
+//}else if (score == 10){
+//    print ("Điểm bạn bằng 10")
+//}
+//else{
+//    print ("Điểm bạn nhỏ hơn 10")
+//    
+//}
+//VD
+var diemTB = 8
+if (diemTB > 8 ) {
+    print ("Hs giỏi")
+}
+else if (diemTB < 8 ) {
+    print ("Hs yếu")
+}
+else if (diemTB == 8 ) {
+    print ("Hs khá")
+}
+
+//--------TOÁN TỬ LOGIC----------
+//--> AND OR NOT
+//1. AND
+
